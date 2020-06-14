a2fadb1defa144d47e90c97ce74c8aab87e79bd5
nandwalritik <ritiknandwal021@gmail.com>
2020-06-14 18:51:41 +0530

    adding comparison of class based implementation of logistic regression and scikitLearn implementation

--- Logistic-Regression/LogisticRegression.py
+++ Logistic-Regression/LogisticRegression.py
@@ -7,19 +7,15 @@ class LogisticRegression:
 		self.m 			= 	Yin.size
 		self.coef_ 		= 	np.random.rand(Xin.shape[1])
 		self.intercept 	=   np.random.rand(1)
-		# self.normalize()
-	def normalize(self):
-		mu 		= np.mean(self.X)
-		sigma 	= np.std(self.X)
-		self.X 	= (self.X-mu)/sigma
+
 	def sigmoid(self,x):
 		g = 1/(1+np.exp(-x))
 		return g
 
 	def computeCost(self,Lambda=0):
 		h_theta 	= 	self.sigmoid(np.dot(self.X,self.coef_)+self.intercept)
-		regularize	=	(Lambda/(2*self.m))*(np.sum(np.square(self.coef_)))
-		J_theta     =   (-1/self.m)*(np.dot(self.Y.T,np.log(h_theta))+np.dot(1-self.Y.T,np.log(1-h_theta))) + regularize
+		# regularize	=	(Lambda/(2*self.m))*(np.sum(np.square(self.coef_)))
+		J_theta     =   -(np.dot(self.Y.T,np.log(h_theta))+np.dot(1-self.Y.T,np.log(1-h_theta)))/(self.m) + regularize
 		return J_theta
 
 	def gradientDescent(self,Lambda=0,alpha=0.01,iterations=2000):
@@ -28,12 +24,27 @@ class LogisticRegression:
 			# print(h.shape)
 			# print(self.Y.shape)
 			# print(self.X.shape)
-			tempCoef_  		=  self.coef_ 		- (alpha/self.m)*(np.dot(self.X.T,h-self.Y)) +(Lambda/(self.m))*self.coef_
+			tempCoef_  		=  self.coef_ 		- (alpha/self.m)*(np.dot(self.X.T,h-self.Y))  +(Lambda/(self.m))*self.coef_
 			tempIntercept	=  self.intercept   - (alpha/self.m)*(np.sum(h-self.Y))
 			self.coef_   	= tempCoef_
 			self.intercept 	= tempIntercept
-			print("Iteration    ",str(i),"   cost   ",str(self.computeCost(Lambda)))
-	
+			# print("Iteration    ",str(i),"   cost   ",str(self.computeCost(Lambda)))
+			# print(self.coef_," ",self.intercept)
+	def stochasticGradientDescent(self,batchSize,Lambda=0,alpha=0.01,iterations=2000,shuffle=True):
+		if shuffle :
+			np.random.shuffle([self.X,self.Y])
+		for i in range(1,iterations):
+			for j in range(0,self.m,batchSize):
+				X_mini = self.X[j:j+batchSize,:]
+				y_mini = self.Y[j:j+batchSize]
+				sz = y_mini.size
+				if(y_mini.size == 0):
+					break
+				h_theta 		= self.sigmoid(np.dot(X_mini,self.coef_)+self.intercept)
+				tempCoef_ 		= self.coef_ - (alpha/sz)*(np.dot(X_mini.T,(h_theta-y_mini)))
+				tempIntercept	= self.intercept  - (alpha/sz)*(np.sum(h_theta-y_mini))
+				self.coef_ = tempCoef_
+				self.intercept	= tempIntercept
 	def predict(self,X):
 		return self.sigmoid(np.dot(X,self.coef_)+self.intercept)>=0.5
 
@@ -45,5 +56,5 @@ class LogisticRegression:
 	def coeficient(self):
 		return [self.coef_]
 
-	def intercept(self):
+	def intercept_(self):
 		return [self.intercept]	
--- Logistic-Regression/logicRegTest.py
+++ Logistic-Regression/logicRegTest.py
@@ -1,27 +1,64 @@
 import numpy as np 
 import LogisticRegression as LR
+import pandas as pd 
+import matplotlib.pyplot as plt
+
+def normalize(X):
+	return (X - np.mean(X,axis=0))/np.std(X,axis=0)
+
 data = np.loadtxt('data1.txt',dtype=float,delimiter=',')
-# print(data.shape)
-# print(data)
+# data = pd.read_csv('Social_Network_Ads.csv') 
 size = data.shape[0]
-testSize = (int)(0.2*size)
+testSize = (int)(0.4*size)
 trainSize = size - testSize
-X = data[:,:2]
+
+X = data[:,:-1]
+normalize(X)
 y = data[:,-1]
 X_train,X_test,y_train,y_test = X[:trainSize],X[trainSize:],y[:trainSize],y[trainSize:]
-# print("X_train")
-# print(X_train)
-# print("X_test")
-# print(X_test)
-# print("y_train")
-# print(y_train)
-# print("y_test")
-# print(y_test)
 
 train = LR.LogisticRegression()
 train.setter(X_train,y_train)
-train.gradientDescent()
 
 test = LR.LogisticRegression()
 test.setter(X_test,y_test)
-print(train.accuracy(test))
+
+train.stochasticGradientDescent(1)
+
+print("Accuracy 		"+str(train.accuracy(test)))
+print("Coefficients 	"+str(train.coeficient()[0]))
+print("Intercept 		"+str(train.intercept_()[0]))
+
+plt.scatter(X_train[:,0],X_train[:,1],c=y_train[:])
+plt.title("Train Set")
+plt.show()
+
+plt.scatter(X_test[:,0],X_test[:,1],c=y_test[:])
+y_dec = -1*(X_train[:,0]*train.coeficient()[0][0]+train.intercept_()[0][0])/train.coeficient()[0][1]
+plt.plot(X_train[:,0],y_dec,c='black',label='Decision Boundary')
+plt.title("Test Set (with decision boundry)")
+plt.legend()
+plt.show()
+
+
+# Scikir-Learn Implementation
+from sklearn.linear_model import LogisticRegression
+
+classifier = LogisticRegression()
+classifier.fit(X_train,y_train)
+y_pred = classifier.predict(X_test)
+accuracy = (sum(y_pred == y_test)/y_test.size)*100
+print("Accuracy 		"+str(accuracy))
+print("Coefficients 	"+str(classifier.coef_[0]))
+print("Intercept 		"+str(classifier.intercept_))
+
+plt.scatter(X_train[:,0],X_train[:,1],c=y_train[:])
+plt.title("Train Set")
+plt.show()
+
+plt.scatter(X_test[:,0],X_test[:,1],c=y_test[:])
+y_dec = -1*(X_train[:,0]*classifier.coef_[0][0]+classifier.intercept_[0])/classifier.coef_[0][1]
+plt.plot(X_train[:,0],y_dec,c='black',label='Decision Boundary')
+plt.title("Test Set (with decision boundry)")
+plt.legend()
+plt.show()
